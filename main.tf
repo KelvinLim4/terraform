c903f09eba0b82a3ed2c311723ed4444d9020d0e
KelvinLima <kelvinlimasouza226@gmail.com>
2022-09-18 11:17:25 -0300

    Definindo tags para deploy do ambiente

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
-provider "aws" {
-region = "us-east-1"
+provider "aws"{
+    region = "us-east-1"
 
 }
 
 resource  "aws_instance" "dev" {
+    count = 3
     ami = "ami-05fa00d4c63e32376"
     instance_type = "t2.micro"
     key_name = "terraform-aws"
+    tags = {
+        Name = "dev${count.index}"
+    }
 }
+
+#para exportar para as credentials para o terraform https://williamalvessantos.medium.com/conectar-terraform-com-a-aws-dc6bd167f6ae

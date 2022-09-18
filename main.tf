8be55d05150e7db9492572aabddc2d66585ec02f
KelvinLima <kelvinlimasouza226@gmail.com>
2022-09-18 10:28:39 -0300

    Provisionando primeira ec2 pelo terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws"{
+provider "aws" {
+region = "us-east-1"
 
+}
 
-
+resource  "aws_instance" "dev" {
+    ami = "ami-05fa00d4c63e32376"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
 }

091240617f99e360cbe62a342dfbebdb9ab682f8
KelvinLima <kelvinlimasouza226@gmail.com>
2023-05-18 11:10:26 -0300

    criando VPC, Subnet e SG e atribuindo para as instancias de dev

--- main.tf
+++ main.tf
@@ -3,19 +3,40 @@ provider "aws" {
 
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/27"
+
+  tags = {
+    Name = "Main"
+  }
+} 
+
 resource  "aws_instance" "dev" {
-    count = 3
-    ami = "ami-05fa00d4c63e32376"
-    instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    count           = 3
+    ami             = "ami-05fa00d4c63e32376"
+    instance_type   = "t2.micro"
+    key_name        = "terraform-aws"
+    subnet_id       =  aws_subnet.main.id
     tags = {
         Name = "dev${count.index}"
     }
+    vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
 }
 
 resource "aws_security_group" "acesso-ssh"{
     name = "acesso-ssh"
     description = "acesso-ssh"
+    vpc_id = aws_vpc.main.id
 
     ingress {
         from_port   = 22
